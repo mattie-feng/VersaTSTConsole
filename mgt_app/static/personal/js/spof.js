0d90f54ecd163844a64caaef5fbead4b10988797
mattie <mattie.feng@feixitek.com>
2022-07-28 15:24:43 +0800

    add upload file function on spof page

--- mgt_app/static/personal/js/spof.js
+++ mgt_app/static/personal/js/spof.js
@@ -1,12 +1,13 @@
 var serverIp = get_vlpx_ip()
 
-layui.use(['form', 'layer'], function () {
+layui.use(['form', 'layer', 'upload'], function () {
   var form = layui.form,
     layer = layui.layer
   // layedit = layui.layedit,
   // laydate = layui.laydate
   var $ = layui.jquery,
     element = layui.element
+  var upload = layui.upload
 
   form.on('submit(spof)', function (data) {
     create_data = JSON.stringify(data.field)
@@ -22,6 +23,22 @@ layui.use(['form', 'layer'], function () {
       console.log(result)
       layer.msg(result)
     })
+  })
+
+  upload.render({
+    elem: '#uploadSpof',
+    url: 'https://httpbin.org/post',
+    accept: 'file',
+    done: function (res) {
+      console.log(res)
+      if (res.code > 0) {
+        return layer.msg('上传失败')
+      }
+      return layer.msg('上传成功')
+    },
+    error: function () {
+      return layer.msg('File upload failed, please try again')
+    }
   })
 })
 
